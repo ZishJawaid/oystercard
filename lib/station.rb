5de3dfe7ddd08632e9be8703eff1874cce20dcae
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-08-13 16:16:40 +0100

    added mocking into tests

--- lib/station.rb
+++ lib/station.rb
@@ -2,9 +2,10 @@ class Station
 
   attr_reader :name, :zone
 
-  def initialize(name="Chelsmford", zone = 6)
+  def initialize(name = "Chelmsford", zone = 1)
     @name = name
     @zone = zone
+
   end
 
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,11 +2,14 @@ require 'station'
 
 describe Station do 
 
-  it 'expects station to have a name' do
-    expect(subject.name).to eq("Chelsmford")
-  end
-  
-  it 'expects station to have a zone' do
-    expect(subject.zone).to eq(6)
-  end
+  subject {described_class.new(name: "Chelmsford", zone: 6)}
+
+   it 'expects station to have a name' do
+     expect(subject.name).to eq("Chelmsford") 
+   end
+
+   it 'expects station to have a name' do
+     expect(subject.zone).to eq(6) 
+   end
+   
 end
