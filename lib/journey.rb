b8bdc4bb75d45167994e4efd24fd6b25e7375e7e
Zish Jawaid <zisheanj@gmail.com>
2020-08-14 19:00:16 +0100

    Added test and implementation for adding penalty fare

--- lib/journey.rb
+++ lib/journey.rb
@@ -4,13 +4,23 @@ attr_reader :journeys
 
   def initialize
     @journeys = {}
-    @start
+    
   end
 
- def start(entry_station = "entry_station")
- @journeys[:entry_station] = entry_station
- p @journeys
- end
-# @journeys[:exit_station] = exit_station 
+  def start(entry_station = nil)
+    @journeys[:entry_station] = entry_station
+  end
+
+  def finish(exit_station = nil)
+    @journeys[:exit_station] = exit_station
+  end
+
+  def fare
+    if @journeys.values_at(:entry_station, :exit_station).all?
+      Card::MINIMUM_FARE
+    else
+      6
+    end
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,6 +2,11 @@ require 'journey'
 
 describe Journey do
   
+   let(:entry_station) { double :entry_station }
+   let(:exit_station) { double :exit_station }
+
+
+
   it 'creates an instance of Journey' do
     expect(subject).to be_an_instance_of Journey
   end
@@ -10,38 +15,65 @@ describe Journey do
     expect(subject.journeys).to be_empty
   end
 
+    describe '#start' do
 
-  #it 'starts a journey' do
-    #oyster = Card.new
-   # expect(subject.journey_start(entry_station)).to eq true
-  #end
-
-  it { respond_to(:start).with(1).argument }
-
-  describe '#start' do
-     let(:entry_station) { double :entry_station }
-    it 'will add the entry station to the journey log' do
+     it { respond_to(:start).with(1).argument }
+     
+     it 'will add the entry station to the journey log' do
       subject.start(entry_station)
       expect(subject.journeys).to include(:entry_station => entry_station)
     end
   end
+  
+
 
   describe '#finish' do
+
+    it { respond_to(:finish).with(1).argument }
+
     it 'wii add the exit station to the journey log' do
-      exit_station = double 
-      entry_station = double
+      
       subject.start(entry_station) 
       subject.finish(exit_station)
       expect(subject.journeys).to include(:entry_station => entry_station, :exit_station => exit_station)
     end
   end
 
-   
-  # let(:journey)  { {entry_station: entry_station, exit_station: exit_station } }
+  describe '#fare' do
+
+    it 'responds to the method fare' do
+      expect(subject).to respond_to :fare
+    end
+
+    it 'should return the mininimum fare' do
+      subject.start(entry_station)
+      subject.finish(exit_station)
+      expect(subject.fare).to eq Card::MINIMUM_FARE
+    end
+
+    it 'should return the penalty fare' do
+      subject.start(entry_station)
+      subject.finish
+      subject.fare
+      expect(subject.fare).to eq 6
+    end
+
+
+  end
+
+
+  
+end
+
+
+
+
+
+
+# let(:journey)  { {entry_station: entry_station, exit_station: exit_station } }
 
   #   it 'stores a journey' do
   #     subject.journey_start(entry_station)
   #     subject.journey_end(exit_station)
   #     expect(subject.journeys).to include journey
   #   end
-end
